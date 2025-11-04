bafb50bd3dd9d8a00f503b4380995da4e25f6949
Supriya Dhekane <dhekanesupriya7@gmail.com>
2025-11-04 15:17:45 +0530

    .env changed

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -1,15 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import  {getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // console.log();
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth=getAuth(app)
-const provider=new GoogleAuthProvider()
-export  {provider,auth}
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { provider, auth };
